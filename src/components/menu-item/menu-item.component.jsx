ac48838508d09e07b023cdd9cc37ff90c3e330ec
Ket Patel <ket.patel610@gmail.com>
2020-09-17 18:29:58 +0530

    used withRouter Higher Order Controler with Menu-Item

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import { withRouter } from "react-router-dom";
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, size, imageUrl }) => (
-    <div className={`${size} menu-item` }>
+const MenuItem = ({ title, size, imageUrl ,history, match,linkUrl }) => (
+    <div className={`${size} menu-item` } onClick={() => history.push(`${match.url}${linkUrl}`)}>
             <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }}  >
 
-                
+
             </div>
                 <div className='content'>
                     <h1 className='title'>{ title.toUpperCase() }</h1>
@@ -15,4 +15,4 @@ const MenuItem = ({ title, size, imageUrl }) => (
             </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
